87313a4d0303341400fe2aed31e8a7ebdfc5c3db
Hector Paulino <hector.x.paulino.-nd@disney.com>
2023-02-09 19:22:53 -0600

    General update

--- script/main.js
+++ script/main.js
@@ -116,6 +116,8 @@ const animationTimeline = () => {
     .to(".idea-4", 0.7, ideaTextTransLeave, "+=2.5")
     .from(".idea-40", 0.7, ideaTextTrans)
     .to(".idea-40", 0.7, ideaTextTransLeave, "+=5")
+    .from(".idea-401", 0.7, ideaTextTrans)
+    .to(".idea-401", 0.7, ideaTextTransLeave, "+=5")
     .from(".idea-41", 0.7, ideaTextTrans)
     .to(".idea-41", 0.7, ideaTextTransLeave, "+=5")
     .from(".idea-42", 0.7, ideaTextTrans)
